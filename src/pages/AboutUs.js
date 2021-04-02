c171ca808a543acd144c7dd276570448b02a1097
mohitkothiyal038 <mohitkothiyal038@gmail.com>
2021-04-02 08:48:24 +0530

    2. aboutuspage

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//Page components
+import About from '../components/About';
+import Services from '../components/Services';
+import FaQ from '../components/FaQ';
+function Aboutus() {
+	return (
+		<>
+			<About />
+			<Services />
+			<FaQ />
+		</>
+	);
+}
+
+export default Aboutus;
