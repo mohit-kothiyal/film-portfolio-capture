5e96f259b3eaf5e14a9ba9748b84ed4bb5be3363
mohitkothiyal038 <mohitkothiyal038@gmail.com>
2021-04-06 07:59:50 +0530

    styled About

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,65 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+import styled from 'styled-components';
 function About() {
 	return (
-		<div>
-			<div className="description">
+		<StyledAbout>
+			<Description>
 				<div className="title">
-					<div className="hide">
+					<Hide>
 						<h2>We work to make</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>
 							your <span>dreams</span>
 						</h2>
-					</div>
-					<div className="hide">
+					</Hide>
+					<Hide>
 						<h2>come true.</h2>
-					</div>
+					</Hide>
 				</div>
 				<p>
 					Contact us for any photography or videography ideas to come to
 					reality.
 				</p>
 				<button>Contact Us</button>
-				<div className="image">
-					<img src={home1} alt="guy with a camera" />
-				</div>
-			</div>
-		</div>
+			</Description>
+			<Image>
+				<img src={home1} alt="guy with a camera" />
+			</Image>
+		</StyledAbout>
 	);
 }
+
+//Styled Components
+const StyledAbout = styled.div`
+	min-height: 90vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 5rem 10rem;
+	color: white;
+`;
+const Description = styled.div`
+	flex: 1;
+	padding-right: 5rem;
+	h2 {
+		font-weight: lighter;
+	}
+`;
+
+const Image = styled.div`
+	flex: 1;
+	overflow: hidden;
+	img {
+		width: 100%;
+		object-fit: cover;
+		height: 80vh;
+	}
+`;
+
+const Hide = styled.div`
+	overflow: hidden;
+`;
 
 export default About;
